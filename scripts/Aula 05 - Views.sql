64143ea1803cebfe346d0b20303c39545d967c24
Miguel Rodrigues Spínola da Hora <mrsdahora@gmail.com>
2026-09-02 11:30:28 +0000

    Materialized views aula

--- scripts/Aula 05 - Views.sql
+++ scripts/Aula 05 - Views.sql
@@ -67,10 +67,104 @@ WITH CHECK OPTION;
 
 -- select * from v_products_in_stock;
 
-update v_products_in_stock
-set estoque = 0
-where id = 1
-returning id, produto, estoque;
+-- update v_products_in_stock
+-- set estoque = 0
+-- where id = 1
+-- returning id, produto, estoque;
 
-insert into v_products_in_stock (produto, valor, estoque)
-values ('Produto qualquer', 99, 0);
+-- insert into v_products_in_stock (produto, valor, estoque)
+-- values ('Produto qualquer', 99, 0);
+
+/*
+MATERIALIZED VIEWS
+*/
+
+-- 5. Relatório de produtos mais vendidos
+drop view if exists v_top_products;
+create view v_top_products as
+select
+    p.id,
+    p.name,
+    sum(op.quantity) unid_vendidas,
+    sum(op.quantity * op.unit_price) total_vendido
+from products p
+join orders_products op on op.product_id = p.id
+join orders o on o.id = op.order_id
+where o.status <> 'canceled'
+group by p.id, p.name;
+
+-- explain analyze select * from v_top_products order by total_vendido desc limit 3;
+
+drop view if exists mv_top_products;
+create materialized view mv_top_products as
+select
+    p.id,
+    p.name,
+    sum(op.quantity) unid_vendidas,
+    sum(op.quantity * op.unit_price) total_vendido
+from products p
+join orders_products op on op.product_id = p.id
+join orders o on o.id = op.order_id
+where o.status <> 'canceled'
+group by p.id, p.name 
+with data; -- padrão
+-- with NO data; -- sem dados
+
+-- explain analyze select * from mv_top_products order by total_vendido desc limit 3;
+
+--                                                                    QUERY PLAN                                                                   
+-- ------------------------------------------------------------------------------------------------------------------------------------------------
+--  Limit  (cost=131.82..131.83 rows=3 width=76) (actual time=36.067..36.071 rows=3 loops=1)
+--    ->  Sort  (cost=131.82..134.02 rows=880 width=76) (actual time=36.066..36.069 rows=3 loops=1)
+--          Sort Key: (sum(((op.quantity)::numeric * op.unit_price))) DESC
+--          Sort Method: top-N heapsort  Memory: 25kB
+--          ->  HashAggregate  (cost=109.44..120.44 rows=880 width=76) (actual time=36.046..36.053 rows=7 loops=1)
+--                Group Key: p.id
+--                Batches: 1  Memory Usage: 49kB
+--                ->  Hash Join  (cost=61.75..92.52 rows=1354 width=56) (actual time=36.025..36.032 rows=12 loops=1)
+--                      Hash Cond: (op.product_id = p.id)
+--                      ->  Hash Join  (cost=31.95..59.14 rows=1354 width=24) (actual time=0.755..0.760 rows=12 loops=1)
+--                            Hash Cond: (op.order_id = o.id)
+--                            ->  Seq Scan on orders_products op  (cost=0.00..23.60 rows=1360 width=28) (actual time=0.497..0.498 rows=13 loops=1)
+--                            ->  Hash  (cost=21.00..21.00 rows=876 width=4) (actual time=0.219..0.220 rows=9 loops=1)
+--                                  Buckets: 1024  Batches: 1  Memory Usage: 9kB
+--                                  ->  Seq Scan on orders o  (cost=0.00..21.00 rows=876 width=4) (actual time=0.211..0.213 rows=9 loops=1)
+--                                        Filter: (status <> 'canceled'::text)
+--                                        Rows Removed by Filter: 1
+--                      ->  Hash  (cost=18.80..18.80 rows=880 width=36) (actual time=35.252..35.253 rows=9 loops=1)
+--                            Buckets: 1024  Batches: 1  Memory Usage: 9kB
+--                            ->  Seq Scan on products p  (cost=0.00..18.80 rows=880 width=36) (actual time=35.230..35.233 rows=9 loops=1)
+--  Planning Time: 38.137 ms
+--  Execution Time: 36.197 ms
+
+--                                                        QUERY PLAN                                                        
+-- -------------------------------------------------------------------------------------------------------------------------
+--  Limit  (cost=27.88..27.89 rows=3 width=76) (actual time=1.275..1.276 rows=3 loops=1)
+--    ->  Sort  (cost=27.88..29.83 rows=780 width=76) (actual time=1.274..1.275 rows=3 loops=1)
+--          Sort Key: total_vendido DESC
+--          Sort Method: top-N heapsort  Memory: 25kB
+--          ->  Seq Scan on mv_top_products  (cost=0.00..17.80 rows=780 width=76) (actual time=1.211..1.213 rows=7 loops=1)
+--  Planning Time: 3.634 ms
+--  Execution Time: 1.455 ms
+
+-- 6. MV para mostrar o total vendido por mês
+drop materialized view if exists mv_monthly_sales;
+create materialized view mv_monthly_sales as
+select
+    to_char(date_trunc('month', o.order_date), 'YYYY-MM') mes,
+    sum(o.total) total_vendido
+from orders o
+where o.status <> 'canceled'
+group by mes
+with no data;
+
+-- select * from mv_monthly_sales order by mes desc;
+
+-- Recarregando a MV (bloqueada durante o refresh...)
+refresh materialized view mv_monthly_sales;
+
+create unique index idx_mv_monthly_sales_month
+on mv_monthly_sales(mes);
+
+-- CONCURENTLY: permite 
+refresh materialized view concurrently mv_monthly_sales;
